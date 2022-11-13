810f70819e6041da962a070cb6d54caa29eee72d
Hernán D. Mercado <hernandmf@gmail.com>
2022-11-13 18:13:36 -0500

    creating the container and the textfield

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Container } from "@mui/material";
+import { Searcher } from "./components/Searcher";
 
 const App = () => {
-  return <div>Soy un astronata</div>;
+  return (
+    <Container
+      sx={{
+        background: "whitesmoke",
+        width: "80vw",
+        height: "500px",
+        borderRadius: "16px",
+        marginTop: "40px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Searcher />
+      <div>Soy un astronata</div>
+    </Container>
+  );
 };
 
 export default App;
---
+++ src/components/Searcher/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Stack, TextField, IconButton } from "@mui/material";
+
+export const Searcher = () => {
+  return (
+    <Stack>
+      <TextField>Perro sucio</TextField>
+      <IconButton></IconButton>
+    </Stack>
+  );
+};
