66a87c979734a667ba1d5be0e9f8db5c285784cb
Hernán D. Mercado <hernandmf@gmail.com>
2022-11-20 19:09:51 -0500

    adding response validation and saving to localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,27 @@ import { gettingUsers } from "./services/users";
 const App = () => {
   const [inputUser, setInputUser] = useState("octocat");
   const [userState, setUserState] = useState("userState");
+  const [notFound, setNotFound] = useState(false);
 
   const getUser = async (user) => {
     const userResponse = await gettingUsers(user);
-    console.log(userResponse);
+
+    if (userState === "octocat") {
+      localStorage.setItem("octocat", userResponse);
+    }
+
+    if (userResponse.message === "Not Found") {
+      const { octocat } = localStorage;
+      setInputUser(octocat);
+      setNotFound(true);
+    } else {
+      setUserState(userResponse);
+    }
   };
 
   useEffect(() => {
     getUser(inputUser);
-  }, []);
+  }, [inputUser]);
   return (
     <Container
       sx={{
